bb8ba9ec32e18a17fe1279bb10ec3268226e40ba
Jakub Smigielski <jakub.smigielski@comarch.com>
2016-11-23 17:48:03 +0100

    Dodanie drugiego wykresu

--- client/js/controllers/OtherCtrl.js
+++ client/js/controllers/OtherCtrl.js
@@ -11,69 +11,69 @@ app.controller('OtherCtrl', function ($scope, $http, $location) {
     function init() {
         $http.get('users').then(function (response) {
             $scope.users = response.data;
-            $(function () {
-                var seriesOptions = [],
-                    seriesCounter = 0,
-                    names = ['MSFT', 'AAPL', 'GOOG'];
+
+            var seriesOptions = [],
+                seriesCounter = 0,
+                names = ['MSFT', 'AAPL', 'GOOG'];
 
 
-                function createChart() {
+            function createChart() {
 
-                    Highcharts.stockChart('container', {
+                Highcharts.stockChart('container', {
 
-                        rangeSelector: {
-                            selected: 4
-                        },
+                    rangeSelector: {
+                        selected: 4
+                    },
 
-                        yAxis: {
-                            labels: {
-                                formatter: function () {
-                                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                                }
-                            },
-                            plotLines: [{
-                                value: 0,
-                                width: 2,
-                                color: 'silver'
-                            }]
-                        },
-
-                        plotOptions: {
-                            series: {
-                                compare: 'percent',
-                                showInNavigator: true
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
                             }
                         },
+                        plotLines: [{
+                            value: 0,
+                            width: 2,
+                            color: 'silver'
+                            }]
+                    },
 
-                        tooltip: {
-                            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                            valueDecimals: 2,
-                            split: true
-                        },
-
-                        series: seriesOptions
-                    });
-                }
-
-                $.each(names, function (i, name) {
-
-                    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?', function (data) {
-
-                        seriesOptions[i] = {
-                            name: name,
-                            data: data
-                        };
-
-                        // As we're loading the data asynchronously, we don't know what order it will arrive. So
-                        // we keep a counter and create the chart when all the data is loaded.
-                        seriesCounter += 1;
-
-                        if (seriesCounter === names.length) {
-                            createChart();
+                    plotOptions: {
+                        series: {
+                            compare: 'percent',
+                            showInNavigator: true
                         }
-                    });
+                    },
+
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2,
+                        split: true
+                    },
+
+                    series: seriesOptions
+                });
+            }
+
+            $.each(names, function (i, name) {
+
+                $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?', function (data) {
+
+                    seriesOptions[i] = {
+                        name: name,
+                        data: data
+                    };
+
+                    // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                    // we keep a counter and create the chart when all the data is loaded.
+                    seriesCounter += 1;
+
+                    if (seriesCounter === names.length) {
+                        createChart();
+                    }
                 });
             });
+
         });
     }
 
