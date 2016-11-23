cfab9f61796c7a0087e926a69b9cc0907d3dfc42
Jakub Smigielski <jakub.smigielski@comarch.com>
2016-11-23 17:52:03 +0100

    Dodanie drugiego wykresu

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -10,53 +10,6 @@ var app = angular.module('app', [
     'ui.bootstrap'
 ]);
 
-app.directive('formGroup', function () {
-    return {
-        restrict: 'A',
-        replace: false,
-        link: function (scope, elem, attrs) {
-
-            attrs.labelCol = attrs.labelCol || 3;
-            attrs.inputCol = attrs.inputCol || 9;
-
-            var element = angular.element(elem);
-
-            var tplOut = '<div class="form-group"></div>';
-
-            var tplIn = '<div class="col-sm-' + attrs.inputCol + '"></div>';
-
-            var label = '<label for="" class="col-sm-' + attrs.labelCol + ' control-label">' + attrs.formGroup + '</label>'
-
-            var el = element.wrap(tplOut).wrap(tplIn).parent().parent().prepend(label);
-
-            scope.$on("$destroy", function () {
-                el.remove();
-            });
-
-        }
-    };
-});
-app.directive('acmeNavbar', function acmeNavbar() {
-    var directive = {
-        restrict: 'E',
-        templateUrl: 'templates/navbar.html',
-        scope: {
-            creationDate: '='
-        },
-        controller: NavbarController,
-        controllerAs: 'vm',
-        bindToController: true
-    };
-
-    return directive;
-
-    function NavbarController() {
-        var vm = this;
-
-        // "vm.creationDate" is available by directive option "bindToController: true"
-    }
-});
-
 /**
  * Created by Marcin on 2016-02-11.
  */
@@ -121,11 +74,11 @@ app.controller('MonitCtrl', function ($scope, $http, $location) {
                     buttons: [{
                         count: 1,
                         type: 'minute',
-                        text: '15M'
+                        text: '1M'
                     }, {
                         count: 5,
                         type: 'minute',
-                        text: '5H'
+                        text: '5M'
                     }, {
                         type: 'all',
                         text: 'All'
@@ -153,8 +106,7 @@ app.controller('MonitCtrl', function ($scope, $http, $location) {
                             var tajm = new Date($scope.users[i].date);
                             data.push([
                                 tajm.getTime(),
-                                parseInt($scope.users[i].name),
-                                parseInt($scope.users[i].name) + 100
+                                parseInt($scope.users[i].name)
                             ]);
                         }
                         return data;
@@ -213,7 +165,117 @@ app.controller('OtherCtrl', function ($scope, $http, $location) {
     function init() {
         $http.get('users').then(function (response) {
             $scope.users = response.data;
+
+            var seriesOptions = [],
+                seriesCounter = 0,
+                names = ['MSFT', 'AAPL', 'GOOG'];
+
+
+            function createChart() {
+
+                Highcharts.stockChart('container', {
+
+                    rangeSelector: {
+                        selected: 4
+                    },
+
+                    yAxis: {
+                        labels: {
+                            formatter: function () {
+                                return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                            }
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 2,
+                            color: 'silver'
+                            }]
+                    },
+
+                    plotOptions: {
+                        series: {
+                            compare: 'percent',
+                            showInNavigator: true
+                        }
+                    },
+
+                    tooltip: {
+                        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        valueDecimals: 2,
+                        split: true
+                    },
+
+                    series: seriesOptions
+                });
+            }
+
+            $.each(names, function (i, name) {
+
+                $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?', function (data) {
+
+                    seriesOptions[i] = {
+                        name: name,
+                        data: data
+                    };
+
+                    // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                    // we keep a counter and create the chart when all the data is loaded.
+                    seriesCounter += 1;
+
+                    if (seriesCounter === names.length) {
+                        createChart();
+                    }
+                });
+            });
+
         });
     }
 
+});
+
+app.directive('formGroup', function () {
+    return {
+        restrict: 'A',
+        replace: false,
+        link: function (scope, elem, attrs) {
+
+            attrs.labelCol = attrs.labelCol || 3;
+            attrs.inputCol = attrs.inputCol || 9;
+
+            var element = angular.element(elem);
+
+            var tplOut = '<div class="form-group"></div>';
+
+            var tplIn = '<div class="col-sm-' + attrs.inputCol + '"></div>';
+
+            var label = '<label for="" class="col-sm-' + attrs.labelCol + ' control-label">' + attrs.formGroup + '</label>'
+
+            var el = element.wrap(tplOut).wrap(tplIn).parent().parent().prepend(label);
+
+            scope.$on("$destroy", function () {
+                el.remove();
+            });
+
+        }
+    };
+});
+app.directive('acmeNavbar', function acmeNavbar() {
+    var directive = {
+        restrict: 'E',
+        templateUrl: 'templates/navbar.html',
+        scope: {
+            creationDate: '='
+        },
+        controller: NavbarController,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+
+    function NavbarController() {
+        var vm = this;
+
+        // "vm.creationDate" is available by directive option "bindToController: true"
+    }
 });
